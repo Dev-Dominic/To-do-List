2e43a0627c309cbd820d4a8ff5a12b8ebdd1b82f
Dominic Henry <dominichenrywork@hotmail.com>
2020-08-12 10:47:08 -0500

    Import fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "../style/App.css";
+import "../styles/App.css";
 import TodoList from "./TodoList.js";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <h1>To-do List</h1>
         <TodoList name="This is my todolist" />
       </div>
     );
